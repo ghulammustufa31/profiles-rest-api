0bb0080fef41ef9a874e10f6e95b770991202906
ghulam.mustufa31 <ghulam.mustufa31@gmail.com>
2019-07-12 21:45:26 +0500

    Added profiles-viewset CRUD

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from . import serializers
 from rest_framework import status
+from . import models
 
 # Create your views here.
 
@@ -100,7 +101,10 @@ class HelloViewSet(viewsets.ViewSet):
         return Response({'http_method':'DELETE'})
 
 
-        
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profiles"""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()        
 
 
-    
